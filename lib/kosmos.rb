9daf23d9b0b36a097858e2d6199f8c79be827744
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-16 16:33:40 -0700

    Have the ksp_path be saved in ~/.kosmos

--- lib/kosmos.rb
+++ lib/kosmos.rb
@@ -6,4 +6,20 @@ require 'kosmos/version'
 module Kosmos
   class InvalidUninstallError < StandardError
   end
+
+  def self.save_ksp_path(path)
+    File.open(config_path, "w+") do |file|
+      file.write(path)
+    end
+  end
+
+  def self.load_ksp_path
+    File.read(config_path)
+  end
+
+  private
+
+  def self.config_path
+    File.join(Dir.home, ".kosmos")
+  end
 end
