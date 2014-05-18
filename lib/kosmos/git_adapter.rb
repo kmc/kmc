3ebd16fd24d477fb065834a0ae5c873f1289f940
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-17 19:30:53 -0700

    Major improvement to GitAdapter.
    Previously, saving state was just comitting everything under Dir["**/*"], but
    that wasn't great. For one, that doesn't work with deletions (they won't show up
    in Dir["**/*"] since they're not in the filesystem anymore), and secondly that
    is incredibly slow since it requires looking through each file, regardless of
    whether that file has been changed or not.
    
    Another issue is that previous implementations did not look at the modes of
    files when comitting them -- they were unconditionally set to some value. Now,
    the actual mode of a file is what is stuck into the git repository. The reason
    for this is because now the files checked in / out come from
    Rugged::Repository#diff_workdir, which takes file modes into consideration.
    
    Hopefully this works!

--- lib/kosmos/git_adapter.rb
+++ lib/kosmos/git_adapter.rb
@@ -11,15 +11,36 @@ module Kosmos
 
         Dir.chdir(repo.workdir) do
           Dir["**/*"].each do |path|
-            stage_file(repo, index, path) unless File.directory?(path)
+            unless File.directory?(path)
+              mode = File.new(path).stat.mode
+              stage_file(repo, index, path, mode)
+            end
           end
         end
 
-        # write changes to index into git database
-        modified_tree = index.write_tree(repo)
-        index.write
+        commit_index(repo, index, commit_message)
+      end
 
-        make_commit(repo, modified_tree, commit_message)
+      def commit_changes(repo_path, commit_message)
+        repo = repo(repo_path)
+        index = repo.index
+        head_tree = repo.lookup(repo.head.target).tree
+
+        diff = repo.diff_workdir(head_tree,
+          include_untracked: true, recurse_untracked_dirs: true)
+
+        diff.each_delta do |delta|
+          if delta.status == :deleted
+            index.remove(delta.old_file[:path])
+          else
+            path = delta.new_file[:path]
+            mode = File::Stat.new(File.join(repo_path, path)).mode
+
+            stage_file(repo, index, path, mode)
+          end
+        end
+
+        commit_index(repo, index, commit_message)
       end
 
       def reset_to_commit(repo_path, commit_oid)
@@ -47,10 +68,16 @@ module Kosmos
         Rugged::Repository.new(path)
       end
 
-      def stage_file(repo, index, path)
+      def stage_file(repo, index, path, mode)
         index.add(path: path,
-          oid: Rugged::Blob.from_workdir(repo, path),
-          mode: 0100644)
+          oid: Rugged::Blob.from_workdir(repo, path), mode: mode)
+      end
+
+      def commit_index(repo, index, message)
+        modified_tree = index.write_tree(repo)
+        index.write
+
+        make_commit(repo, modified_tree, message)
       end
 
       def make_commit(repo, tree, message)
