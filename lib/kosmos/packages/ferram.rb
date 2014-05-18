aac453be8c2023b20b935d56c5e2a7eab8fd4bb4
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-17 23:45:41 -0700

    Add a working package for Ferram Aerospace Research.
    I have installed and uninstalled this mod a few dozen times today.

--- lib/kosmos/packages/ferram.rb
+++ lib/kosmos/packages/ferram.rb
@@ -4,4 +4,9 @@ class FerramAerospaceResearch < Kosmos::Package
 
   homepage 'http://forum.kerbalspaceprogram.com/threads/20451-0-23-5-Ferram-Aerospace-Research-v0-13-3-4-30-14'
   url 'https://github.com/ferram4/Ferram-Aerospace-Research/releases/download/v0.13.3/FerramAerospaceResearch_v0_13_3.zip'
+
+  def install
+    merge_directory 'GameData'
+    merge_directory 'Ships'
+  end
 end
