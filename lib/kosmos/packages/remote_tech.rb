5b6838c71d77678d7f6d32503a9ffb05ed116e03
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-20 19:31:35 -0700

    Add a package for Remote Tech 2.
    Also included is the community hotfix for the latest versions of KSP.

---
+++ lib/kosmos/packages/remote_tech.rb
@@ -0,0 +1,18 @@
+class RemoteTech2 < Kosmos::Package
+  title 'Remote Tech 2'
+  url 'http://kerbalspaceport.com/wp/wp-content/themes/kerbal/inc/download.php?f=uploads/2013/12/RemoteTech2_Release_1.3.3.zip'
+
+  def install
+    merge_directory 'GameData'
+  end
+end
+
+class RemoteTech2Fixpack < Kosmos::Package
+  title 'Remote Tech 2 - Fixpack for KSP 0.23.5'
+  aliases 'remote tech 2 fixpack'
+  url 'http://download1819.mediafire.com/svbfnd092h3g/dncc8qu44t30a90/RemoteTech2_2014.01.08.22.30.zip'
+
+  def install
+    merge_directory 'GameData'
+  end
+end
