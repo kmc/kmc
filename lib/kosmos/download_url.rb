6fb522d5cf43df59f583849f9dbd2b58fa98b21b
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-26 01:06:40 -0700

    Use PhantomJS to fetch mediafire link urls.

--- lib/kosmos/download_url.rb
+++ lib/kosmos/download_url.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Kosmos
   class DownloadUrl
     attr_reader :url
@@ -6,8 +8,16 @@ module Kosmos
       @url = url
     end
 
+    def resolve_download_url
+      html = Nokogiri::HTML(rendered_html)
+
+      if mediafire?
+        html.css('.download_link a').first['href']
+      end
+    end
+
     def mediafire?
-      url =~ /mediafire\.com/
+      url =~ /mediafire/
     end
 
     def box?
@@ -16,6 +26,14 @@ module Kosmos
 
     def dropbox?
       url =~ /dropbox/
+    end
+
+    private
+
+    def rendered_html
+      Dir.chdir(File.dirname(__FILE__)) do
+        `phantomjs page_fetcher.js #{url}`
+      end
     end
   end
 end
--- spec/download_url_spec.rb
+++ spec/download_url_spec.rb
@@ -6,6 +6,13 @@ describe Kosmos::DownloadUrl do
     expect(Kosmos::DownloadUrl.new(url)).to be_mediafire
   end
 
+  it 'correctly resolves mediafire download urls' do
+    url = 'file://' + File.absolute_path('spec/fixtures/example_mediafire.html')
+    target_url = 'http://download1690.mediafire.com/qny71y9k6kvg/o6cbe03iitggj1p/B9+Aerospace+Pack+R4.0c.zip'
+
+    expect(Kosmos::DownloadUrl.new(url).resolve_download_url).to eq target_url
+  end
+
   it 'detects box links' do
     url = 'https://app.box.com/s/some-random-stuff'
     expect(Kosmos::DownloadUrl.new(url)).to be_box
