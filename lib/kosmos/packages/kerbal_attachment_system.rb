077c9ddc7610b4b456fc5152bc78ef389ce4c7fb
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-31 14:57:21 -0700

    KAS is now hosted on nabaal.net

--- lib/kosmos/packages/kerbal_attachment_system.rb
+++ lib/kosmos/packages/kerbal_attachment_system.rb
@@ -1,7 +1,7 @@
 class KerbalAttachmentSystem < Kosmos::Package
   title 'Kerbal Attachment System'
   aliases 'kas'
-  url 'http://kerbalspaceport.com/wp/wp-content/themes/kerbal/inc/download.php?f=uploads/2014/04/KAS_v0.4.7.zip'
+  url 'https://nabaal.net/files/ksp/KAS_v0.4.7.zip'
 
   def install
     merge_directory 'GameData'
