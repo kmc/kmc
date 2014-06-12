222537dd10b448e53f2288b7424c62e6a898269a
Ulysse Carion <ulyssecarion@gmail.com>
2014-06-12 16:14:02 -0700

    Add option for downloader to save to cache.

--- lib/kosmos/package_downloads.rb
+++ lib/kosmos/package_downloads.rb
@@ -3,8 +3,8 @@ module Kosmos
     # Downloads and unzips a package. This will call #download! on its own, and
     # will return the location where the package was downloaded to as a
     # Pathname.
-    def self.download_and_unzip_package(package)
-      download_file = download_package(package)
+    def self.download_and_unzip_package(package, opts = {})
+      download_file = download_package(package, opts)
 
       Util.log "Unzipping ..."
 
@@ -30,19 +30,21 @@ module Kosmos
     # is found first. Uses DownloadUrl to intelligently resolve download URLs.
     #
     # Returns the file downloaded, which is created in a temp directory.
-    def self.download_package(package)
+    def self.download_package(package, opts = {})
       cached_download = download_from_cache(package)
       downloaded_file = if cached_download
         Util.log "Use a cached version of #{package.title} ..."
         cached_download
       else
-        Util.log "The package is found at #{package.url}. \
-          Finding the download URL ..."
+        Util.log "The package is found at #{package.url}."\
+          "Finding the download URL ..."
         download_url = DownloadUrl.new(package.url).resolve_download_url
 
         Util.log "Found it. Downloading from #{download_url} ..."
         HTTParty.get(download_url)
       end
+
+      save_to_cache(package, downloaded_file) if opts[:cache_after_download]
 
       tmpdir = Dir.mktmpdir
 
@@ -61,6 +63,17 @@ module Kosmos
         cached_download = File.join(cache_dir, "#{package.title}.zip")
 
         File.read(cached_download) if File.file?(cached_download)
+      end
+    end
+
+    def self.save_to_cache(package, downloaded_file)
+      Util.log "Saving #{package.title} to cache ..."
+
+      cache_dir = Kosmos.cache_dir
+      if cache_dir
+        File.open(File.join(cache_dir, "#{package.title}.zip"), 'w') do |file|
+          file.write(downloaded_file)
+        end
       end
     end
   end
