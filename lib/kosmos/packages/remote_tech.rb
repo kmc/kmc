51b2003c7b0cab4352931d48b71192edadaf74cf
Ulysse Carion <ulyssecarion@gmail.com>
2014-06-17 10:00:08 -0700

    RemoteTech2 has been updated.

--- lib/kosmos/packages/remote_tech.rb
+++ lib/kosmos/packages/remote_tech.rb
@@ -1,16 +1,6 @@
 class RemoteTech2 < Kosmos::Package
   title 'Remote Tech 2'
-  url 'https://github.com/RemoteTechnologiesGroup/RemoteTech/releases/download/v1.3.3/RemoteTech-v1.3.3.zip'
-
-  def install
-    merge_directory 'GameData'
-  end
-end
-
-class RemoteTech2Fixpack < Kosmos::Package
-  title 'Remote Tech 2 - Fixpack for KSP 0.23.5'
-  aliases 'remote tech 2 fixpack'
-  url 'http://www.mediafire.com/download/dncc8qu44t30a90/RemoteTech2_2014.01.08.22.30.zip'
+  url 'https://github.com/RemoteTechnologiesGroup/RemoteTech/releases/download/v1.4.0/RemoteTech-v1.4.0.zip'
 
   def install
     merge_directory 'GameData'
