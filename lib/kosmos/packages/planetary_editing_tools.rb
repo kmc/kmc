eed7f7546e58a6cde80eccbe7c991ef2fa2396b8
Robin Appelman <icewind@owncloud.com>
2014-07-03 19:40:35 +0200

    formating

--- lib/kosmos/packages/planetary_editing_tools.rb
+++ lib/kosmos/packages/planetary_editing_tools.rb
@@ -3,7 +3,6 @@ class PlanetaryEditingTools < Kosmos::Package
   url 'https://dl.dropboxusercontent.com/u/80556647/PlanetaryEditingToolsUI.zip'
 
   def install
-    merge_directory 'KittopiaSpace',
-      into: 'GameData'
+    merge_directory 'KittopiaSpace', into: 'GameData'
   end
 end
