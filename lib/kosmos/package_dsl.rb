179eefbb4ca7afbb2585f9d1815168eef5aa25d7
Robin Appelman <icewind@owncloud.com>
2014-07-04 00:37:59 +0200

    add the options for packages to remove file or folders in their install scripts

--- lib/kosmos/package_dsl.rb
+++ lib/kosmos/package_dsl.rb
@@ -6,5 +6,9 @@ module Kosmos
       FileUtils.cp_r(File.join(@download_dir, from),
         File.join(@ksp_path, destination))
     end
+    
+    def remove(path, opts = {})
+      FileUtils.rm_rf(File.join(@ksp_path, path))
+    end
   end
 end
