54828da8011cfd6dd10fc1eb7ce9055b3e6658eb
Robin Appelman <icewind@owncloud.com>
2014-07-08 19:13:10 +0200

    add MapShowNavBall

---
+++ lib/kosmos/packages/map_show_nav_ball.rb
@@ -0,0 +1,10 @@
+class MapShowNavBall < Kosmos::Package
+  title 'MapShowNavBall'
+  aliases 'Map Show Nav Ball'
+  url 'http://kerbal.curseforge.com/ksp-mods/221011-atmospheric-sound-enhancement'
+
+  def install
+    merge_directory 'GameData'
+  end
+end
+
