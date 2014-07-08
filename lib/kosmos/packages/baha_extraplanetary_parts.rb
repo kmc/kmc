1f624f7b3f8db7754bcbb51a4f0dbd211ea291eb
Ulysse Carion <ulyssecarion@gmail.com>
2014-07-08 11:17:17 -0700

    Improve the package for BahamutoD's parts.

--- lib/kosmos/packages/baha_extraplanetary_parts.rb
+++ lib/kosmos/packages/baha_extraplanetary_parts.rb
@@ -1,5 +1,7 @@
 class BahaExtraplanetaryParts < Kosmos::Package
-  title 'Baha Extraplanetary Parts'
+  title 'BahamutoD\'s Drills and Parts for EL'
+  aliases 'BahamutoDs drills and parts', 'el drills and parts',
+    'baha extraplanetary parts'
   url 'http://www.mediafire.com/download/cyn3bnpsy9p0z4g/bahaELpartsv1.2.zip'
 
   def install
