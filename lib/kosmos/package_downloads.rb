aeb52d8d0b8abf15a215242dbfe1795e18809d4c
Ulysse Carion <ulyssecarion@gmail.com>
2014-07-09 16:26:57 -0700

    Refactor and bugfix package_downloads.
    Moved a lot of stuff into their own methods.
    
    Handled the case of a mod having non-URI-friendly characters in their name when
    they were downlaoded from the cache.

--- lib/kosmos/package_downloads.rb
+++ lib/kosmos/package_downloads.rb
@@ -32,55 +32,82 @@ module Kosmos
     #
     # Returns the file downloaded, which is created in a temp directory.
     def self.download_package(package, opts = {})
-      # TODO: Refactor this to make it less yucky.
-
-      cached_download, download_uri = download_from_cache(package)
-      downloaded_file = if cached_download
-        Util.log "Use a cached version of #{package.title} ..."
-        cached_download
-      else
-        Util.log "The package is found at #{package.url}. "\
-          "Finding the download URL ..."
-        download_uri = DownloadUrl.new(package.url).resolve_download_url
-
-        Util.log "Found it. Downloading from #{download_uri} ..."
-        HTTParty.get(download_uri)
-      end
+      download_uri, downloaded_file = fetch_package_file(package)
 
       save_to_cache(package, downloaded_file) if opts[:cache_after_download]
 
-      tmpdir = Dir.mktmpdir
-
-      download_file_name = File.basename(URI(download_uri).path)
-      download_file = File.new(File.join(tmpdir, download_file_name), 'wb+')
-      download_file.write(downloaded_file)
-      download_file.close
-
-      download_file
+      download_to_tempdir(download_file_name(download_uri), downloaded_file)
     end
 
     private
 
-    def self.download_from_cache(package)
-      cache_dir = Kosmos.cache_dir
-      if cache_dir
-        cached_download = File.join(cache_dir, "#{package.title}.zip")
+    # Given a package, returns a two-entry array.
+    #
+    # The first entry is a String uri pointing to where the file was fetched
+    # from (either the filesystem or a website), and the second entry is the
+    # contents of the download.
+    def self.fetch_package_file(package)
+      cache = cache_file(package)
+      if cache
+        Util.log "Using a cached version of #{package.title} ..."
 
-        if File.file?(cached_download)
-          [File.read(cached_download), cached_download]
-        end
+        [cache, File.read(cache)]
+      else
+        Util.log "The package is found at #{package.url}."
+        Util.log "Finding the download URL ..."
+
+        download_uri = resolve_download_url(package)
+
+        Util.log "Found it. Downloading from #{download_uri} ..."
+        [download_uri, HTTParty.get(download_uri)]
       end
     end
 
+    # Returns the location of the cached version of a package, or some falsy
+    # value if no such file exists.
+    def self.cache_file(package)
+      if Kosmos.cache_dir
+        cache_file = File.join(Kosmos.cache_dir, "#{package.title}.zip")
+
+        File.file?(cache_file) && cache_file
+      end
+    end
+
+    def self.resolve_download_url(package)
+      DownloadUrl.new(package.url).resolve_download_url
+    end
+
+    # Writes a downloaded package to the cache.
+    #
+    # This method assumes `Kosmos.cache_dir` already exists.
     def self.save_to_cache(package, downloaded_file)
       Util.log "Saving #{package.title} to cache ..."
 
-      cache_dir = Kosmos.cache_dir
-      if cache_dir
-        File.open(File.join(cache_dir, "#{package.title}.zip"), 'wb+') do |file|
-          file.write(downloaded_file)
-        end
+      cache_location = File.join(Kosmos.cache_dir, "#{package.title}.zip")
+      File.open(cache_location, 'wb+') do |file|
+        file.write(downloaded_file)
       end
+    end
+
+    # Converts a string URI into the file name a downloaded file should be
+    # placed into.
+    def self.download_file_name(uri)
+      # Remove non-alphanumeric characters to ensure a valid URI.
+      clean_uri = uri.gsub(/[^[:alnum:]]/, '')
+
+      File.basename(URI(clean_uri).path)
+    end
+
+    # Places `file_contents` into a file called `file_name` in a temporary
+    # directory.
+    def self.download_to_tempdir(file_name, file_contents)
+      tmpdir = Dir.mktmpdir
+
+      file = File.new(File.join(tmpdir, file_name), 'wb+')
+      file.write(file_contents)
+      file.close
+
+      file
     end
   end
 end
