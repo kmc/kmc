1a927be64e38b134d2bd43a63f85d201eccd1a9b
Robin Appelman <icewind@owncloud.com>
2014-07-11 00:43:55 +0200

    add RLA Stockalike and friends

---
+++ lib/kosmos/packages/rla_stockalike.rb
@@ -0,0 +1,27 @@
+class RLAStockalike < Kosmos::Package
+  title 'RLA Stockalike'
+  url 'http://kerbal.curseforge.com/ksp-mods/220907-rla-stockalike-archive-mirror'
+
+  def install
+    merge_directory 'RLA Stockalike/GameData', into: '.'
+  end
+end
+
+class RLAElectricEngines < Kosmos::Package
+  title 'RLA Electric Engines'
+  url 'http://kerbal.curseforge.com/ksp-mods/220907-rla-stockalike-archive-mirror'
+
+  def install
+    merge_directory 'RLA Electric Engines/GameData', into: '.'
+  end
+end
+
+class RLAPowerGeneration < Kosmos::Package
+  title 'RLA Power Generation'
+  url 'http://kerbal.curseforge.com/ksp-mods/220907-rla-stockalike-archive-mirror'
+
+  def install
+    merge_directory 'RLA Power Generation/GameData', into: '.'
+  end
+end
+
