486755fb64bd90c68c554846da3ac31c3412e925
Ulysse Carion <ulyssecarion@gmail.com>
2014-07-10 18:13:29 -0700

    Move config stuff to Kosmos::Configuration, and move config location.
    Config is now at ~/.kosmos/config.json

---
+++ lib/kosmos/configuration.rb
@@ -0,0 +1,62 @@
+module Kosmos
+  module Configuration
+    class Configurator
+      attr_accessor :verbose, :post_processors, :output_method
+
+      def initialize
+        @verbose = false
+        @post_processors = [Kosmos::PostProcessors::ModuleManagerResolver]
+        @output_method = Proc.new { |str| puts str }
+      end
+    end
+
+    class << self
+      def save_ksp_path(path)
+        write_config(ksp_path: path)
+      end
+
+      def load_ksp_path
+        read_config[:ksp_path]
+      end
+
+      def cache_dir
+        read_config[:cache_dir]
+      end
+
+      private
+
+      def write_config(opts)
+        ensure_config_exists!
+
+        config_to_write = JSON.pretty_generate(read_config.merge(opts))
+        File.open(config_path, "w") do |file|
+          file.write config_to_write
+        end
+      end
+
+      def read_config
+        ensure_config_exists!
+
+        config = File.read(config_path)
+        if config.empty?
+          {}
+        else
+          JSON.parse(config, symbolize_names: true)
+        end
+      end
+
+      def ensure_config_exists!
+        FileUtils.mkdir_p(kosmos_path)
+        FileUtils.touch(config_path)
+      end
+
+      def config_path
+        File.join(kosmos_path, 'config.json')
+      end
+
+      def kosmos_path
+        File.join(Dir.home, ".kosmos")
+      end
+    end
+  end
+end
