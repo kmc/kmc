2f71c2cc629ab1ff9831d94307c7cccc20585b2a
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-13 01:36:40 +0200

    Add support to remove file or path

--- lib/kosmos/package_dsl.rb
+++ lib/kosmos/package_dsl.rb
@@ -6,5 +6,8 @@ module Kosmos
       FileUtils.cp_r(File.join(self.class.download_dir, from),
         File.join(self.class.ksp_path, destination))
     end
+    def remove_filepath(filepath)
+      FileUtils.rm_r(File.join(self.class.ksp_path, filepath))
+    end
   end
 end
