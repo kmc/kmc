430c990535add07d767006e4343155d3e70d5e36
shrx <differenxe@gmail.com>
2014-07-13 17:15:07 +0200

    Update kerbin_cup.rb
    possible fix #396

--- lib/kosmos/packages/kerbin_cup.rb
+++ lib/kosmos/packages/kerbin_cup.rb
@@ -1,7 +1,7 @@
 class KerbinCup < Kosmos::Package
   title 'Kerbin Cup'
   
-  url 'http://www.curse.com/ksp-mods/kerbal/221319-kerbin-cup'
+  url 'http://kerbal.curseforge.com/ksp-mods/221319-kerbin-cup'
 
   def install
     merge_directory 'Kerbin Cup', into: 'GameData'
