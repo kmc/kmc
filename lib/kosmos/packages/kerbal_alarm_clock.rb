8ac2b232750d2dd52abe0afeb588681c7bd9ec02
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-07-13 13:26:23 +0200

    Update Kerbal Alarm Clock to 2.7.6.0

--- lib/kosmos/packages/kerbal_alarm_clock.rb
+++ lib/kosmos/packages/kerbal_alarm_clock.rb
@@ -1,8 +1,8 @@
 class KerbalAlarmClock < Kosmos::Package
   title 'Kerbal Alarm Clock'
-  url 'https://github.com/TriggerAu/KerbalAlarmClock/releases/download/v2.7.3.0/KerbalAlarmClock_2.7.3.0.zip'
+  url 'https://github.com/TriggerAu/KerbalAlarmClock/releases/download/v2.7.6.0/KerbalAlarmClock_2.7.6.0.zip'
 
   def install
-    merge_directory 'KerbalAlarmClock_2.7.3.0/GameData'
+    merge_directory 'KerbalAlarmClock_2.7.6.0/GameData'
   end
 end
