2a7157a64e08c65f3aeef3d084c0db73b7b82b0c
shrx <differenxe@gmail.com>
2014-07-13 14:38:44 +0200

    Update remote_tech.rb
    Add aliases `RT`, `RT2`, `remotetech`, `remotetech2`

--- lib/kosmos/packages/remote_tech.rb
+++ lib/kosmos/packages/remote_tech.rb
@@ -1,5 +1,7 @@
 class RemoteTech2 < Kosmos::Package
   title 'Remote Tech 2'
+  aliases 'RT', 'RT2', 'remotetech', 'remotetech2'
+  
   url 'https://github.com/RemoteTechnologiesGroup/RemoteTech/releases/download/v1.4.0/RemoteTech-v1.4.0.zip'
 
   def install
