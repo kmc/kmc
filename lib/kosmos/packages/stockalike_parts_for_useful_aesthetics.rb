cab7bdb4a69f7ea29b5bf39cbe8c7caee2e02e82
shrx <differenxe@gmail.com>
2014-07-13 16:53:21 +0200

    Update stockalike_parts_for_useful_aesthetics.rb
    fix url

--- lib/kosmos/packages/stockalike_parts_for_useful_aesthetics.rb
+++ lib/kosmos/packages/stockalike_parts_for_useful_aesthetics.rb
@@ -2,7 +2,7 @@ class TurboNisuStockalikeParts < Kosmos::Package
   title 'Stockalike parts for useful esthetics'
   aliases 'Stockalike parts for useful aesthetics', 'TurboNisu stockalike parts', 'TurboNisu aesthetic parts pack'
   
-  url 'http://download1694.mediafire.com/z304dlewf6cg/et4ctfdh592a6sn/TurboNisuPartsPack1.02.rar'
+  url 'http://www.mediafire.com/download/et4ctfdh592a6sn/TurboNisuPartsPack1.02.rar'
 
   def install
     merge_directory 'GameData'
