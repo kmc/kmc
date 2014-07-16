ec63a4784272bb05557ef92ec0ee9bd75f86a060
shrx <differenxe@gmail.com>
2014-07-16 10:23:58 +0200

    Update b9_iva_extension.rb
    Add alias `B9 crew tank internals`; fix #308

--- lib/kosmos/packages/b9_iva_extension.rb
+++ lib/kosmos/packages/b9_iva_extension.rb
@@ -1,5 +1,7 @@
 class B9IVAExtension < Kosmos::Package
   title 'B9 IVA Extension'
+  aliases 'B9 crew tank internals'
+  
   url 'http://www.mediafire.com/download/x64zpyd65toepcc/B9_IVA_Extension_0.5.1.zip'
 
   def install
