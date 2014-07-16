8cca06b49ac2c36436930e255bd8eb2f1a98c21f
shrx <differenxe@gmail.com>
2014-07-16 18:38:58 +0200

    Delete glowstrips.rb
    looks like we already have this

--- lib/kosmos/packages/glowstrips.rb
+++
@@ -1,8 +0,0 @@
-class GlowStrips < Kosmos::Package
-  title 'GlowStrips'
-  url 'http://kerbal.curseforge.com/ksp-mods/221036-glowstrips-v0-1'
-
-  def install
-    merge_directory 'GlowStrips', into: 'GameData'
-  end
-end
