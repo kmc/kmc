0fa390c773d74c8402e5bb1785d1a74bc04192b2
shrx <differenxe@gmail.com>
2014-07-16 18:38:12 +0200

    Create glowstrips.rb
    fix #354

---
+++ lib/kosmos/packages/glowstrips.rb
@@ -0,0 +1,8 @@
+class GlowStrips < Kosmos::Package
+  title 'GlowStrips'
+  url 'http://kerbal.curseforge.com/ksp-mods/221036-glowstrips-v0-1'
+
+  def install
+    merge_directory 'GlowStrips', into: 'GameData'
+  end
+end
