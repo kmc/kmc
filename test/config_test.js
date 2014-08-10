1c128c057f923b136562d41d45b7881d19cb7605
Ulysse Carion <ulyssecarion@gmail.com>
2014-08-10 22:16:12 +0200

    Add a starting version for getKmcPath.

---
+++ test/config_test.js
@@ -0,0 +1,31 @@
+var should = require('should');
+var mock = require('mock-fs');
+
+var Config = require('../lib/config');
+
+describe('Config', function() {
+  // Make sure the real `fs` module is returned after each test.
+  afterEach(function() {
+    mock.restore();
+  });
+
+  // Very un-DRY to repeat this from config.js, but I see no alternative.
+  var home = process.env.HOME || process.env.USERPROFILE;
+  var configPath = home + '/.kmc/config.json';
+
+  describe('#readKspPath', function() {
+    it('reads from a JSON file at ~/.kmc/config.json', function(done) {
+      var newFs = {};
+      newFs[configPath] = '{ "kspPath": "/path/to/kmc" }';
+
+      mock(newFs);
+
+      Config.loadKspPath(function(err, result) {
+        if (err) { throw err; }
+
+        result.should.eql('/path/to/kmc');
+        done();
+      });
+    });
+  });
+});
