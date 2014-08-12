f66a98ae2fd8749cb25b4bbab4c24280c2b4d723
Ulysse Carion <ulyssecarion@gmail.com>
2014-08-12 23:40:16 +0200

    Test for a commit being made by GitAdapter#commitEverything.
    This involves quite a few modifications to git_adapter_test.js

--- test/git_adapter_test.js
+++ test/git_adapter_test.js
@@ -12,22 +12,22 @@ describe('GitAdapter', function() {
     return ChildProcess.execAsync('cd ' + path + ';' + cmd);
   }
 
+  // A promise to set up the KSP directory for testing. Make sure this promise
+  // returns the temporary directory created.
+  var setUpKspPath = temp.mkdirAsync('ksp').then(function(tempDir) {
+    return fs.writeFileAsync(tempDir + '/a.txt', 'lorem').then(function() {
+      return tempDir;
+    });
+  });
+
+  // A promise to set up KSP and init in the tempdir. Returns the tempdir.
+  var initAndChdir = setUpKspPath.then(function(tempDir) {
+    return GitAdapter.initRepo(tempDir).then(function() {
+      return tempDir;
+    });
+  });
+
   describe('#initRepo', function() {
-    // A promise to set up the KSP directory for testing. Make sure this promise
-    // returns the temporary directory created.
-    var setUpKspPath = temp.mkdirAsync('ksp').then(function(tempDir) {
-      return fs.writeFileAsync(tempDir + '/a.txt', 'lorem').then(function() {
-        return tempDir;
-      });
-    });
-
-    // A promise to set up KSP and init in the tempdir. Returns the tempdir.
-    var initAndChdir = setUpKspPath.then(function(tempDir) {
-      return GitAdapter.initRepo(tempDir).then(function() {
-        return tempDir;
-      });
-    });
-
     it('creates a repo at the path passed', function(done) {
       initAndChdir.then(function(tempDir) {
         var inGitDir = 'git rev-parse --is-inside-work-tree';
@@ -37,6 +37,24 @@ describe('GitAdapter', function() {
 
           done();
         });
+      });
+    });
+  });
+
+  describe('#commitEverything', function() {
+    it('commits everything as the KMC user', function(done) {
+      var tempDir;
+
+      initAndChdir.then(function(kspTempDir) {
+        tempDir = kspTempDir;
+
+        return GitAdapter.commitEverything(tempDir, 'Test message');
+      }).then(function() {
+        // Count how many commits have been made.
+        return execInPath(tempDir, 'git rev-list HEAD --count');
+      }).then(function(stdout, stderr) {
+        parseInt(stdout).should.eql(1);
+        done();
       });
     });
   });
