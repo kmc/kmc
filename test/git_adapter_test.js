19b6087d8a25c4002a1bf02a2118e62efe8ea164
Ulysse Carion <ulyssecarion@gmail.com>
2014-08-13 00:17:18 +0200

    Add GitAdapter#listCommits.
    The tests are modified to assure that a new temporary directory is created each
    time.

--- test/git_adapter_test.js
+++ test/git_adapter_test.js
@@ -1,6 +1,7 @@
 var Promise = require('bluebird');
 var should = require('should');
 var chdir = require('chdir');
+var _ = require('lodash');
 var temp = Promise.promisifyAll(require('temp').track());
 var fs = Promise.promisifyAll(require('fs'));
 var ChildProcess = Promise.promisifyAll(require('child_process'));
@@ -14,24 +15,28 @@ describe('GitAdapter', function() {
 
   // A promise to set up the KSP directory for testing. Make sure this promise
   // returns the temporary directory created.
-  var setUpKspPath = temp.mkdirAsync('ksp').then(function(tempDir) {
-    return fs.writeFileAsync(tempDir + '/a.txt', 'lorem').then(function() {
-      return tempDir;
+  var setUpKspPath = function() {
+    return temp.mkdirAsync('ksp').then(function(tempDir) {
+      return fs.writeFileAsync(tempDir + '/a.txt', 'lorem').then(function() {
+        return tempDir;
+      });
     });
-  });
+  }
 
   // A promise to set up KSP and init in the tempdir. Returns the tempdir.
-  var initAndChdir = setUpKspPath.then(function(tempDir) {
-    return GitAdapter.initRepo(tempDir).then(function() {
-      return tempDir;
+  var initAndChdir = function() {
+    return setUpKspPath().then(function(tempDir) {
+      return GitAdapter.initRepo(tempDir).then(function() {
+        return tempDir;
+      });
     });
-  });
+  }
 
   describe('#initRepo', function() {
     it('creates a repo at the path passed', function(done) {
       var tempDir;
 
-      initAndChdir.then(function(kspTempDir) {
+      initAndChdir().then(function(kspTempDir) {
         tempDir = kspTempDir;
 
         return execInPath(tempDir, 'git rev-parse --is-inside-work-tree');
@@ -47,7 +52,7 @@ describe('GitAdapter', function() {
     it('commits everything as the KMC user', function(done) {
       var tempDir;
 
-      initAndChdir.then(function(kspTempDir) {
+      initAndChdir().then(function(kspTempDir) {
         tempDir = kspTempDir;
 
         return GitAdapter.commitEverything(tempDir, 'Test message');
@@ -60,6 +65,30 @@ describe('GitAdapter', function() {
         return execInPath(tempDir, 'git log -1 --pretty="%an"');
       }).then(function(stdout, stderr) {
         stdout.join('').should.eql('KMC\n');
+
+        done();
+      });
+    });
+  });
+
+  describe('#listCommits', function() {
+    it('returns a list of commit messages and shas', function(done) {
+      var tempDir;
+
+      initAndChdir().then(function(kspTempDir) {
+        tempDir = kspTempDir;
+
+        return GitAdapter.commitEverything(tempDir, 'hello');
+      }).then(function() {
+        return GitAdapter.commitEverything(tempDir, 'bonjour');
+      }).then(function() {
+        return GitAdapter.commitEverything(tempDir, 'hola');
+      }).then(function() {
+        return GitAdapter.listCommits(tempDir);
+      }).then(function(commits) {
+        _.map(commits, function(commit) {
+          return commit.split(' ')[1];
+        }).should.eql(['hola', 'bonjour', 'hello']);
 
         done();
       });
