aac6c7b77b23af06b0e3bc45126a992c0e38ce8a
Ulysse Carion <ulyssecarion@gmail.com>
2014-08-13 22:43:27 +0200

    Make DownloadUrlResolver a module, not a class.
    Though this makes internal code arguably un-DRY, it reduces boilerplate for
    everday client-facing code.

--- test/download_url_resolver_test.js
+++ test/download_url_resolver_test.js
@@ -9,16 +9,19 @@ describe('DownloadUrlResolver', function() {
     it('detects mediafire links', function() {
       var url = 'http://www.mediafire.com/download/some-random-stuff/whatever.zip';
 
-      (new DownloadUrlResolver(url)).isMediafire().should.be.true;
+      DownloadUrlResolver.isMediafire(url).should.be.true;
     });
 
+    // TODO: Figure out how to get this test to work, given that request doesn't
+    // like "URLs" that use the `file://` protocol.
+    //
     // it('resolves mediafire links', function(done) {
     //   this.timeout(0);
 
     //   var url = 'file://' + __dirname + '/fixtures/mediafire.com.html';
     //   var targetUrl = 'http://download1690.mediafire.com/qny71y9k6kvg/o6cbe03iitggj1p/B9+Aerospace+Pack+R4.0c.zip';
 
-    //   (new DownloadUrlResolver(url)).resolve().then(function(url) {
+    //   DownloadUrlResolver.resolve(url).then(function(url) {
     //     url.should.eql(targetUrl);
 
     //     done();
@@ -43,7 +46,7 @@ describe('DownloadUrlResolver', function() {
         });
 
 
-      (new DownloadUrlResolver(url)).resolve().then(function(downloadUrl) {
+      DownloadUrlResolver.resolve(url).then(function(downloadUrl) {
         // with a direct download URL, the original URL can be downloaded from
         // directly.
         downloadUrl.should.eql(url);
@@ -57,16 +60,19 @@ describe('DownloadUrlResolver', function() {
     it('detects dropbox links', function() {
       var url = 'https://www.dropbox.com/s/some-random-stuff/whatever.zip';
 
-      (new DownloadUrlResolver(url)).isDropbox().should.be.true;
+      DownloadUrlResolver.isDropbox(url).should.be.true;
     });
 
+    // This spec is commented out because PhantomJS is slow and it can take
+    // multiple seconds to get this to run.
+    //
     // it('resolves dropbox links', function(done) {
     //   this.timeout(0);
 
     //   var url = 'file://' + __dirname + '/fixtures/dropbox.com.html';
     //   var targetUrl = 'https://dl.dropboxusercontent.com/s/od4kickxt92jpo2/BetterAtmosphereV4%5BREL%5D.zip?dl=1&token_hash=AAFn5emxuVXLw_RfjDgQs0Hn7-YZ-vejn3m8zLgOj2tTFA&expiry=1401095304';
 
-    //   (new DownloadUrlResolver(url)).resolve().then(function(url) {
+    //   DownloadUrlResolver.resolve(url).then(function(url) {
     //     url.should.eql(targetUrl);
 
     //     done();
@@ -78,7 +84,7 @@ describe('DownloadUrlResolver', function() {
     it('detects curseforge links', function() {
       var url = 'http://kerbal.curseforge.com/plugins/123123-whatever';
 
-      (new DownloadUrlResolver(url)).isCurseforge().should.be.true;
+      DownloadUrlResolver.isCurseforge(url).should.be.true;
     });
   });
 
@@ -86,7 +92,7 @@ describe('DownloadUrlResolver', function() {
     it('detects box links', function() {
       var url = 'https://app.box.com/s/89skim2e3simjwmuof4c';
 
-      (new DownloadUrlResolver(url)).isBox().should.be.true;
+      DownloadUrlResolver.isBox(url).should.be.true;
     });
 
     it('resolves box links', function(done) {
@@ -98,7 +104,7 @@ describe('DownloadUrlResolver', function() {
         .get('/s/89skim2e3simjwmuof4c')
         .reply(200, fs.readFileSync(pagePath, 'utf8'));
 
-      (new DownloadUrlResolver(url)).resolve().then(function(url) {
+      DownloadUrlResolver.resolve(url).then(function(url) {
         url.should.eql(targetUrl);
 
         done();
