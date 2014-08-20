1a0ff6574b46d0413d8975e3c8a687ae08424fb7
Ulysse Carion <ulyssecarion@gmail.com>
2014-08-20 10:11:45 -0700

    Add YAML deserialization of packages.

--- test/package_test.js
+++ test/package_test.js
@@ -3,6 +3,38 @@ var should = require('should');
 var Package = require('../lib/package');
 
 describe('Package', function() {
+  describe('#loadFromYaml', function() {
+    it('reads in a YAML file', function() {
+      yaml = [
+        '- name: Name',
+        '  aliases: [alias]',
+        '  prerequisites: [prereq]',
+        '  postrequisites: [postreq]',
+        '  url: url',
+        '  decompression_method: none',
+        '  install:',
+        '    - merge_directory: [From, To]',
+        '  caveats: |',
+        '    Caveat message'
+      ].join('\n');
+
+      packages = Package.loadFromYaml(yaml);
+
+      packages.length.should.eql(1);
+
+      packages[0].name.should.eql('Name');
+      packages[0].aliases.should.eql(['alias']);
+      packages[0].prerequisites.should.eql(['prereq']);
+      packages[0].postrequisites.should.eql(['postreq']);
+      packages[0].url.should.eql('url');
+      packages[0].decompressionMethod.should.eql('none');
+      packages[0].installProcedure.should.eql([
+        {'merge_directory': ['From', 'To']}
+      ]);
+      packages[0].caveats.should.eql('Caveat message\n');
+    });
+  });
+
   describe('#normalizeName', function() {
     it('removes capitalization', function() {
       Package.normalizeName('Example').should.eql('example');
